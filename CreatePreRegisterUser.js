5406475e045b48a86e4a9817f7e1629cdeb5e774
= <=>
2023-04-25 19:49:55 -0300

    first commit

---
+++ CreatePreRegisterUser.js
@@ -0,0 +1,48 @@
+function CreatePreRegisterUser() {
+    require('dotenv').config();
+    const name = document.querySelector('#name')
+    const lastName = document.querySelector('#lastName')
+    const user = document.querySelector('#lastName')
+    const url = 'https://integracao.dli.minhabiblioteca.com.br/DigitalLibraryIntegrationService/CreatePreRegisterUser';
+    const apiKey = ''
+
+    const requestXml =
+        `<?xml version="1.0" encoding="utf-8"?>
+            <CreatePreRegisterUserRequest
+            xmlns="http://dli.zbra.com.br"
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <FirstName>${name.value}</FirstName>
+            <LastName>${lastName.value}</LastName>
+            <UserName>${user.value}</UserName>
+        </CreatePreRegisterUserRequest>`;
+
+    const headers = {
+        'Content-Type': 'application/xml; charset=utf-8',
+        'X-DigitalLibraryIntegration-API-Key': apiKey,
+        'Host': 'integracao.dli.minhabiblioteca.com.br'
+    };
+
+    const requestOptions = {
+        method: 'POST',
+        headers,
+        body: requestXml
+    };
+
+    async function sendRequest() {
+        try {
+            const response = await fetch(url, requestOptions);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const responseText = await response.text();
+            console.log(responseText);
+            const responseContainer  = document.createElement('div')
+            responseContainer.innerText = responseText
+            document.body.appendChild(responseContainer)
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    }
+    sendRequest()
+}
